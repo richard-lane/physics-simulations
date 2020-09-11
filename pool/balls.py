6cd83d52b95fb2234d5a382409752e7559b60379
Richard Lane <richard.lane@bristol.ac.uk>
2020-09-11 22:32:08 +0100

    ball drawing code

--- pool/balls.py
+++ pool/balls.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 
 
 class Balls:
@@ -9,20 +10,24 @@ class Balls:
 
     """
 
-    x = np.array()
-    y = np.array()
-    v_x = np.array()
-    v_y = np.array()
-    r = np.array()
+    x = np.array([])
+    y = np.array([])
+    v_x = np.array([])
+    v_y = np.array([])
+    r = np.array([])
     num_balls = 0
 
     def __init__(self, num_balls):
+        """
+        Create some randomly positioned, sized and directed balls
+
+        """
         self.num_balls = num_balls
-        self.x = np.zeros(num_balls)
-        self.y = np.zeros(num_balls)
-        self.v_x = np.zeros(num_balls)
-        self.v_y = np.zeros(num_balls)
-        self.r = np.zeros(num_balls)
+        self.x = np.random.randint(0, 100, size=num_balls)
+        self.y = np.random.randint(0, 100, size=num_balls)
+        self.v_x = np.random.randint(-10, 10, size=num_balls)
+        self.v_y = np.random.randint(-10, 10, size=num_balls)
+        self.r = np.random.randint(0, 10, size=num_balls)
 
     def move(self, x_limits, y_limits):
         """
@@ -38,12 +43,24 @@ class Balls:
 
         # Process collisions with the edges
         for i in range(self.num_balls):
-            if self.x[i] < x_limits[0] or self.x > x_limits[1]:
+            if self.x[i] < x_limits[0] or self.x[i] > x_limits[1]:
                 self.x[i] -= 2 * self.v_x[i]
+                self.v_x[i] *= -1
 
         # Might be quicker to do it all in one loop
         # but then again it might not, because of the cache
         for i in range(self.num_balls):
-            if self.y[i] < y_limits[0] or self.y > y_limits[1]:
+            if self.y[i] < y_limits[0] or self.y[i] > y_limits[1]:
                 self.y[i] -= 2 * self.v_y[i]
+                self.v_y[i] *= -1
+
+    def draw(self, screen):
+        """
+        Draw these balls on a Pygame screen
+
+        """
+        for i in range(self.num_balls):
+            pygame.draw.circle(
+                screen, (255, 255, 255), [self.x[i], self.y[i]], self.r[i]
+            )
 
---
+++ pool/draw.py
@@ -0,0 +1,38 @@
+import pygame
+
+from . import balls
+
+def draw():
+    pygame.init()
+
+    x_bounds = [0, 700]
+    y_bounds = [0, 500]
+    size = [x_bounds[1], y_bounds[1]]
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption("Drawing test")
+    done = False
+
+    clock = pygame.time.Clock()
+
+    TestBalls = balls.Balls(10)
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        
+        # Move balls
+        TestBalls.move(x_bounds, y_bounds)
+
+        # Draw background and balls
+        screen.fill((0, 0, 0))
+        TestBalls.draw(screen)
+
+        # Limit FPS
+        clock.tick(60)
+
+        # Update screen
+        pygame.display.flip()
+    
+    pygame.quit()
