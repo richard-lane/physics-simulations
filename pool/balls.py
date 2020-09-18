ad6cab83b85a2cf92673ef6ce500e98a05cb041d
Richard Lane <richard.lane@bristol.ac.uk>
2020-09-18 20:53:11 +0100

    make collisions work ( a bit )

--- pool/balls.py
+++ pool/balls.py
@@ -26,10 +26,10 @@ class Balls:
         # TODO should track how many balls are moving and place stationary balls at the end of the list
         # Then we only need to check the first few balls for collisions
         # It might be inefficienct to move things around too much, but it might save a lot of time if only a few balls are moving
-        self.num_balls = 2
-        self.positions = np.array([[200, 350], [250, 350]])
-        self.velocities = np.array([[1, 0], [-1, 0]])
-        self.radii = np.random.randint(0, 10, size=self.num_balls)
+        self.num_balls = num_balls
+        self.positions = np.random.randint(0, 500, size=(self.num_balls, 2))
+        self.velocities = np.random.randint(0, 5, size=(self.num_balls, 2))
+        self.radii = np.random.randint(4, 10, size=self.num_balls)
         self.masses = np.square(self.radii)
 
     def draw(self, screen):
@@ -39,7 +39,10 @@ class Balls:
         """
         for i in range(self.num_balls):
             pygame.draw.circle(
-                screen, (255, 255, 255), self.positions[i].astype(int), self.radii[i]
+                screen,
+                (255, 255, 255),
+                np.ceil(self.positions[i]).astype(int),
+                self.radii[i],
             )
 
     def colliding(self, i: int, j: int) -> bool:
@@ -53,7 +56,7 @@ class Balls:
 
         """
         # Stationary balls don't collide
-        if not np.any(self.velocities[i]) or not np.any(self.velocities[j]):
+        if not np.any(self.velocities[i]) and not np.any(self.velocities[j]):
             return False
 
         return (self.positions[i][0] - self.positions[j][0]) ** 2 + (
@@ -65,8 +68,12 @@ class Balls:
         Resolve a collision between particles i and j
 
         """
-        # When balls collide, they bounce off each other
-        v1_new, v2_new = collisions.non_relativistic_collision(
+        # TODO use move api
+        # TODO move the right amount
+        self.positions[i] -= 2 * self.velocities[i]
+        self.positions[j] -= 2 * self.velocities[j]
+
+        self.velocities[i], self.velocities[j] = collisions.non_relativistic_collision(
             self.velocities[i],
             self.velocities[j],
             self.positions[i],
@@ -74,9 +81,6 @@ class Balls:
             self.masses[i],
             self.masses[j],
         )
-
-        self.velocities[i] = v1_new
-        self.velocities[j] = v2_new
 
     def move(self, x_limits, y_limits):
         """
--- pool/collisions.py
+++ pool/collisions.py
@@ -11,17 +11,19 @@ def non_relativistic_collision(v1, v2, x1, x2, m1, m2):
 
     Only works with 2d velocities
 
+    I dont think this conserves momentum...
+
     """
     if len(v1) != 2 or len(v2) != 2 or len(x1) != 2 or len(x2) != 2:
         raise ValueError("Only handles 2d velocities")
 
     # Some things that will be helpful for our calculation
     mass_sum = m1 + m2
-    dv_dot_dx = (np.array(v1) - np.array(v2)).dot(np.array(x1) - np.array(x2))
-    dx = np.array(x1) - np.array(x2)
+    dx = x1 - x2
+    dv = v1 - v2
 
     # I stole a formula from Wikipedia
-    delta_v1 = (2 * m2 / mass_sum) * (dv_dot_dx / (dx.dot(dx))) * dx
-    delta_v2 = -(2 * m1 / mass_sum) * (dv_dot_dx / (dx.dot(dx))) * dx
+    delta_v1 = (2 * m2 / mass_sum) * (dv.dot(dx) / (dx.dot(dx))) * dx
+    delta_v2 = -(2 * m1 / mass_sum) * (dv.dot(dx) / (dx.dot(dx))) * dx
 
     return v1 - delta_v1, v2 - delta_v2
